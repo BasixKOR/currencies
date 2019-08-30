42968d7a30a14950e9dca7d338e661fc7c6027f8
Basix <basix@basix.tech>
2019-08-30 17:33:06 +0900

    Add Bulma, SASS for styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  return <div className="App">
+  </div>;
+};
 
 export default App;
