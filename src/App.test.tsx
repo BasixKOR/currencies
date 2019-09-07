855503d46e5701238e8b22fba4e70819e82df8bb
Basix <basix@basix.tech>
2019-09-07 23:53:49 +0900

    Fix JSX factory to be in scope

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { jsx } from '@emotion/core';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
