26b0ec111cf62e38ac4c1c6644203dcf9275f3f0
Basix <basix@basix.tech>
2019-09-08 02:18:39 +0900

    Add Rate domain and related

--- src/store.ts
+++ src/store.ts
@@ -23,3 +23,35 @@ export const selection = SelectionDomain.store<Selection>({
   .on(changedUnit, (state, unit) => ({ ...state, unit }))
   .on(changedTarget, (state, target) => ({ ...state, target }));
 
+selection.watch(state => {
+  getRates(state);
+});
+
+interface Rates {
+  [currency: string]: {
+    price: number; // The price calculated through unit
+    unitPrice: number; // The price of unit currency, calcuated in `currency`
+  };
+}
+
+export const RateDomain = createDomain('rates domain');
+export const getRates = RateDomain.effect<Selection, Rates, Error>(
+  'get rates'
+).use(async selection => {
+  const res = await fetch(
+    `https://api.exchangeratesapi.io/latest?base=${selection.unit}`
+  );
+  const json = await res.json(); // json.rates contains key-value pair of currency symbol and price of unit currency
+  const data: Rates = {};
+  for (const rate in json.rates)
+    data[rate] = {
+      price: json.rates[selection.target] * (1 / json.rates[rate]),
+      unitPrice: json.rates[rate],
+    };
+  return data;
+});
+
+export const rates = RateDomain.store<Rates>({}).on(
+  getRates.done,
+  (_, { result }) => ({ ...result })
+);
