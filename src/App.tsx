477d36c9c9ddfdad96e80a87ceea82aa22a86af6
Basix <basix@basix.tech>
2019-09-07 23:23:38 +0900

    Remove styled API
    I thought it could imporve my code by using styled API,
    but as it turns out the original Emotion API can achive similar result.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { jsx, css } from '@emotion/core';
-import styled from '@emotion/styled';
 
 /** @jsx jsx */
 
-const Hero = styled.section`
+const Hero = css`
   background-image: linear-gradient(
       0deg,
       rgba(33, 150, 83, 0.5),
@@ -26,11 +25,11 @@ const Hero = styled.section`
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Hero>
+      <section css={Hero}>
         <h1 css={css({ fontWeight: 300 })}>
           시시각각 변하는 환율 정보를 바로 확인하세요.
         </h1>
-      </Hero>
+      </section>
     </div>
   );
 };
