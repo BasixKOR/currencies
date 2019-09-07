d97734ff179c92f7f5ff051d30a91213eda51fa0
Basix <basix@basix.tech>
2019-09-07 21:27:18 +0900

    Remove bulma
    Since I designed this project on Figma, I thought I don't actually need to
    use a CSS framework so I put it out.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { jsx, css } from '@emotion/core';
+
 /** @jsx jsx */
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <section
-        className="hero is-medium is-bold"
         css={css`
           background-image: linear-gradient(
               0deg,
@@ -15,18 +15,15 @@ const App: React.FC = () => {
             ),
             url(./dollar.jpg);
           background-size: cover;
+          height: 30vh;
         `}
       >
-        <div className="hero-body">
-          <div className="container">
-            <h1
-              className="title"
-              css={css({ textAlign: 'center', color: 'white' })}
-            >
-              시시각각 변하는 환율 정보를 바로 확인하세요.
-            </h1>
-          </div>
-        </div>
+        <h1
+          className="title"
+          css={css({ textAlign: 'center', color: 'white' })}
+        >
+          시시각각 변하는 환율 정보를 바로 확인하세요.
+        </h1>
       </section>
     </div>
   );
