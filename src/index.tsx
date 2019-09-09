a810122c1261677f1828559d5f9a9d7dff1c20ef
Basix <basix@basix.tech>
2019-09-09 15:13:32 +0900

    WORKAROUND: Avoid react/react-in-jsx-scope lint error

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { jsx } from'@emotion/core'
 import * as serviceWorker from './serviceWorker';
 import './style.scss';
+
+/** @jsx jsx */
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
